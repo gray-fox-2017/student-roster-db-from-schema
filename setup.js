270162dd6f99ddf1294b20146ededeaed0a59075
jamessraun <jeezybig@gmail.com>
2017-05-08 14:18:00 +0700

    student roster db done

--- setup.js
+++ setup.js
@@ -1,3 +1,39 @@
 "use strict"
 
-//write your code here
+const repl = require('repl');
+const sqlite = require('sqlite3').verbose();
+
+var replServer = repl.start({
+  prompt:'$ '
+})
+
+
+var file = 'student.db'
+var db = new sqlite.Database(file);
+
+var create_table = "create table if not exists student (id INTEGER primary key autoincrement, first_name VARCHAR(100) not null,last_name VARCHAR(100),gender VARCHAR(25),birthdate DATE,email VARCHAR(100),phone VARCHAR(15));";
+var seed_data = "insert into student (first_name,last_name,gender,birthdate,email,phone) values ('Rubi','Henjaya','Pria','1986-11-20','rubi@haha.com','0834234342342'),('Riza','Fahmi','Pria','1990-01-10','riza@haha.com','0857212356554');";
+
+let createTable = () => {
+  db.serialize(() => {
+    db.run(create_table,err =>{
+      if(err)
+        console.log(err);
+      else console.log('table created')
+    });
+  });
+}
+
+
+let seedData = () =>{
+  db.serialize( ()=> {
+    db.run(seed_data,err =>{
+      if(err)
+        console.log(err);
+      else console.log('table seeded');
+    });
+  })
+}
+
+replServer.context.createTable = createTable;
+replServer.context.seedTable = seedData;
--- student_roster.js
+++ student_roster.js
@@ -2,5 +2,169 @@
 
 const repl = require('repl');
 const sqlite = require('sqlite3').verbose();
+let replServer = repl.start({
+  prompt:'$ '
+});
+let db =new sqlite.Database('student.db');
 
-// write your code here
+class Student {
+  constructor() {
+    this.data=this;
+  }
+
+  insertTable(firstname,lastname,gender,birthday,email,phone){
+
+    let query=`insert into student (first_name,last_name,gender,birthdate,email,phone) values ('${firstname}','${lastname}','${gender}','${birthday}','${email}','${phone}');`;
+
+    db.serialize(() => {
+      db.run(query,err =>{
+          if(err)
+          console.log(err)
+          else console.log(`date inserted`);
+      });
+    });
+  }
+
+  getAll(){
+
+    let query = "select * from student";
+
+    db.serialize(() => {
+      db.all(query,(err,rows) =>{
+        if(err)
+        console.log(err);
+        else {
+          console.log('\n');
+          for(let i=0; i<rows.length;i++)
+            console.log(rows[i].id,rows[i].first_name,rows[i].last_name,rows[i].gender,rows[i].birthdate,rows[i].email,rows[i].phone);
+        }
+      });
+    });
+  }
+
+  updateTable(id,firstname,lastname,gender,birthday,email,phone){
+    //let data=this.data;
+    let query = `update student set first_name='${firstname}',last_name='${lastname}',gender='${gender}',birthdate='${birthday}',email='${email}',phone='${phone}' where id=${id};`
+
+    db.serialize(() => {
+      db.run(query,err =>{
+          if(err)
+          console.log(err);
+          else{
+            console.log(`data updated!`);
+          //  data.getAll();
+          }
+      });
+    });
+  }
+
+  deleteTable(id){
+    //let data=this.data;
+    let query = `delete from student where id=${id};`;
+
+    db.serialize(() => {
+      db.run(query,err =>{
+          if(err)
+          console.log(err);
+          else {
+            console.log(`data Deleted!`);
+          }
+
+      });
+    });
+    //data.getAll();
+  }
+
+  getByName(name){
+
+    let query = `select * from student where first_name='${name}' or last_name='${name}'`;
+
+    db.serialize(() => {
+      db.all(query,(err,rows) =>{
+        if(err)
+        console.log(err);
+        else {
+          console.log('\n');
+          for(let i=0; i<rows.length;i++)
+            console.log(rows[i].id,rows[i].first_name,rows[i].last_name,rows[i].gender,rows[i].birthdate,rows[i].email,rows[i].phone);
+        }
+      });
+    });
+  }
+
+  getAllByInput(column,value){
+
+    let query = `select * from student where ${column}='${value}'`;
+
+    db.serialize(() => {
+      db.all(query,(err,rows) =>{
+        if(err)
+        console.log(err);
+        else {
+          console.log('\n');
+          for(let i=0; i<rows.length;i++)
+            console.log(rows[i].id,rows[i].first_name,rows[i].last_name,rows[i].gender,rows[i].birthdate,rows[i].email,rows[i].phone);
+        }
+      });
+    });
+  }
+
+  getByThisMonth(){
+
+    let query = `select * from student where strftime('%m', birthdate) = strftime('%m', 'now')`;
+
+    db.serialize(() => {
+      db.all(query,(err,rows) =>{
+        if(err)
+        console.log(err);
+        else {
+          console.log(`B'day this Month:`)
+          for(let i=0; i<rows.length;i++)
+            console.log(rows[i].id,rows[i].first_name,rows[i].last_name,rows[i].gender,rows[i].birthdate,rows[i].email,rows[i].phone);
+        }
+      });
+    });
+  }
+
+    getBirthdateAsc(){
+
+      let query = `select * from student order by strftime('%m-%d', birthdate) asc`;
+
+      db.serialize(() => {
+        db.all(query,(err,rows) =>{
+          if(err)
+          console.log(err);
+          else {
+            console.log(`B'day this Year:`)
+            for(let i=0; i<rows.length;i++)
+              console.log(rows[i].id,rows[i].first_name,rows[i].last_name,rows[i].gender,rows[i].birthdate,rows[i].email,rows[i].phone);
+          }
+        });
+      });
+    }
+
+  help(){
+    console.log("\n============ HELP ============");
+    console.log("$ insertTable(firstname,lastname,gender,birthday,email,phone)")
+    console.log("$ updateTable()");
+    console.log("$ deleteTable()");
+    console.log("$ getAll()");
+    console.log("$ getByName(name)");
+    console.log("$ getAllByInput(column,value)");
+    console.log("$ getByThisMonth()");
+    console.log("$ help()");
+  }
+
+}
+
+let student = new Student()
+
+replServer.context.insertTable = student.insertTable;
+replServer.context.getAll = student.getAll;
+replServer.context.updateTable = student.updateTable;
+replServer.context.deleteTable = student.deleteTable;
+replServer.context.getByName = student.getByName;
+replServer.context.getAllByInput = student.getAllByInput;
+replServer.context.getByThisMonth = student.getByThisMonth;
+replServer.context.getBirthdateAsc = student.getBirthdateAsc;
+replServer.context.help = student.help;
