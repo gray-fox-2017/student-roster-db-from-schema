664d2ef7ff962b63607c7ebd5d199290fe327b83
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-05-11 22:03:58 +0700

    Student Roaster Done

--- setup.js
+++ setup.js
@@ -1,3 +1,52 @@
 "use strict"
 
 //write your code here
+const repl = require('repl');
+const sqlite3 = require('sqlite3').verbose();
+var file = 'student.db';
+var db = new sqlite3.Database(file);
+
+//SQl Statement
+var CREATE_TABEL = "CREATE TABLE students(Id Integer Primary Key,first_name VARCHAR(100), last_name VARCHAR(100), gender varchar(25),birthday Date, email VARCHAR(100),phone VARCHAR)";
+
+var SEED_DATA = "INSERT INTO students (Id,first_name,last_name,gender,birthday,email,phone) VALUES (01,'Jumadi', 'Akhmad', 'Male', 1991-02-27,'jumadialkhmad2@gmail.com', 081380515932),(02,'Ade', 'Nugraha','Male', 1990-06-21,'adenugraha2@gmail.com',081312311322)";
+
+const replServer = repl.start({
+  prompt: '>',
+  input : process.stdin,
+  output : process.stdout
+});
+
+
+//CREATE_TABEL
+let createTabel = () => {
+  //Run SQL One at a time
+  db.serialize(function () {
+    //create tabel
+    db.run(CREATE_TABEL, function(err) {
+      if(err){
+        console.log(err);
+      } else {
+        console.log('CREATE TABEL');
+      }
+    })
+  })
+
+}
+
+let seedData = () => {
+  db.serialize(function () {
+    //create tabel
+    db.run(SEED_DATA, function(err) {
+      if(err){
+        console.log(err);
+      } else {
+        console.log('INSERT VALUE');
+      }
+    })
+  })
+}
+
+
+replServer.context.createTabel=createTabel;
+replServer.context.seedData=seedData;
--- student_roster.js
+++ student_roster.js
@@ -1,6 +1,144 @@
 "use strict"
 
 const repl = require('repl');
-const sqlite = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
+var file = 'student.db';
+var db = new sqlite3.Database(file);
 
-// write your code here
+// write your code dhere
+const replServer = repl.start({
+  prompt: '>',
+  input: process.stdin,
+  output: process.stdout
+});
+
+class Students {
+  static addStudents (Id,first_name, last_name ,gender, birthday, email,phone ){
+    let add = `INSERT INTO students (first_name, last_name,gender, birthday ,email , phone) VALUES ('${first_name}', '${last_name}','${gender}' , '${birthday}' , '${email}' ,'${phone}');`;
+      db.serialize(function () {
+      db.run(add, function (err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log(add);
+        }
+      });
+    });
+  }
+
+
+   static updateStudents(Id,first_name,last_name, gender , birthday , email , phone) {
+     let update = `UPDATE students SET first_name = '${first_name}', last_name = '${last_name}', gender = '${gender}',
+                  birthday = '${birthday}', email = '${email}', phone = '${phone}' WHERE Id = '${Id}'`;
+      db.serialize(function() {
+
+        db.run(update, function (err,row) {
+          if(err) {
+            console.log(err);
+          } else {
+          console.log(row);
+        }
+      })
+    })
+  }
+
+  static deleteStudents (Id) {
+    let del = `DELETE FROM students WHERE students.id = '${Id}';`;
+      db.serialize(function () {
+      db.run(del, function (err,row) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log(row);
+        }
+      });
+    });
+  }
+
+  static showStudents(Id , first_name , last_name , gender , birthday , email , phone){
+    let show = `SELECT * FROM students`;
+      db.serialize(function () {
+        db.all(show, function (err,row) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log(row);
+          }
+        });
+      });
+    }
+
+  static showStudentsName(name) {
+    let findName = `SELECT * FROM students WHERE first_name LIKE '%${name}%' OR last_name LIKE '%${name}%';`
+      db.serialize(function () {
+        db.all(findName, function (err, row) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(row);
+          }
+        });
+      });
+    }
+
+  static findAttribute(attribute, value){
+    let show = `SELECT * FROM students WHERE ${attribute} = '${value}'`;
+      db.serialize(function(){
+        db.all(show, function(err,row){
+          if(err){
+            console.log(err);
+          }else {
+            console.log(row);
+          }
+        });
+      });
+    }
+    static findBirthdayByThisMonth () {
+
+     let findBirtdhay = `SELECT * FROM students WHERE strftime('%m', birthday = strftime('%m', 'now')`;
+     db.serialize(function () {
+       db.each(findBirthday, function (err, row) {
+         if (err) {
+           console.log(err);
+         }
+         else {
+           console.log(row);
+         }
+       });
+     });
+   }
+
+   static sortBirthday () {
+     let sortBirthday = `SELECT * FROM students ORDER BY birthday desc`;
+     db.serialize(function () {
+       db.each(sortBirthday, function (err, row) {
+         if (err) {
+           console.log(err);
+         }
+         else {
+           console.log(row);
+         }
+       });
+     });
+   }
+
+   static help () {
+     let help = `showStudent ()\naddStudent (firstname, lastname, birthdate)\nupdateStudent (id, attribute, value)\ndeleteStudent (id)\nfindName (name)\nfindAttribute (attribute)\nfindBirthdayByThisMonth ()\nsortBirthday ()\nhelp ()`;
+     console.log(help);
+   }
+
+
+}
+
+replServer.context.addStudents=Students.addStudents;
+replServer.context.updateStudents= Students.updateStudents;
+replServer.context.deleteStudents=Students.deleteStudents;
+replServer.context.showStudents=Students.showStudents;
+replServer.context.findAttribute=Students.findAttribute;
+replServer.context.showStudentsName=Students.showStudentsName;
+replServer.context.findBirthdayByThisMonth=Students.findBirthdayByThisMonth;
+replServer.context.sortBirthday=Students.sortBirthday;
+replServer.context.help=Students.help;
